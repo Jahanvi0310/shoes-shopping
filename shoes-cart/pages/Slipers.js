d95fbb32779ab5d03df0e499df16774a356542c4
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2023-02-15 22:23:05 +0530

    First commit

---
+++ shoes-cart/pages/Slipers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slipers = () => {
+  return (
+    <div>
+      <h1>Slipers</h1>
+    </div>
+  )
+}
+
+export default Slipers
---
+++ shoes-cart/pages/Sneaker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sneaker = () => {
+  return (
+    <div>
+      <h1>Sneaker</h1>
+    </div>
+  )
+}
+
+export default Sneaker
